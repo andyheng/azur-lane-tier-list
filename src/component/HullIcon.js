b0da563932f66fc9e2f861e2bddfdececec64655
Andy <andy.dot.heng@gmail.com>
2018-02-16 17:32:44 -0500

    initialize hull icon component

---
+++ src/component/HullIcon.js
@@ -0,0 +1,27 @@
+import React from "react"
+import CV from "../images/hullIcons/CV.png";
+import BB from "../images/hullIcons/BB.png";
+import CL from "../images/hullIcons/CL.png";
+import CA from "../images/hullIcons/CA.png";
+import DD from "../images/hullIcons/DD.png";
+import AR from "../images/hullIcons/AR.png";
+import BBV from "../images/hullIcons/BBV.png";
+
+const HullIcon = (props) => {
+  const HullIcons = {
+    AR,
+    BB,
+    BC: BB,
+    BBV: BBV,
+    CA,
+    CL,
+    CV,
+    CVL: CV,
+    DD
+  }
+  return (
+    <img src={HullIcons[props.hullType]} />
+  )
+}
+
+export default HullIcon;
