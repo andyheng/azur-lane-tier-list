ffbfac33890a4d80bea78b3515a909869915a90e
Andy <andy.dot.heng@gmail.com>
2018-02-16 17:33:35 -0500

    add hullicon, details components

--- src/component/Ship.js
+++ src/component/Ship.js
@@ -1,7 +1,17 @@
 import React from "react";
+import HullIcon from "./HullIcon";
+import ShipDetails from "./ShipDetails";
 
-const Ship = ({name}) => (
-  <p>{name}</p>
+// still need rarity background.
+const Ship = ({name, src, hullType, nation, rarity, tier, details}) => (
+  <div className="item-container">
+    <div>
+      <img src={src} />
+      <HullIcon hullType={hullType} />
+      <ShipDetails details={details} />
+    </div>
+    <h3>{name}</h3>
+  </div>
 )
 
 export default Ship;
