af89571902e3f4e5690d91816f077afe6c2f96f2
Andy <andy.dot.heng@gmail.com>
2018-02-16 17:18:33 -0500

    sort mapped items

--- src/component/TierRow.js
+++ src/component/TierRow.js
@@ -5,12 +5,10 @@ const TierRow = (props) => (
   <Fragment>
     <h2>{props.rowTitle}</h2>
     {
-      props.filteredDataByRows.map(ship => (
-        <Ship 
-          {...ship}
-          key={ship._id}
-        />
-      ))
+      // create ship item, sort by rarity, then sort by name
+      props.filteredDataByRows
+        .map(ship => (<Ship {...ship} key={ship._id}/>))
+        .sort((a, b) => b.props.rarity.localeCompare(a.props.rarity) || a.props.name.localeCompare(b.props.name))
     }
   </Fragment>
 )
