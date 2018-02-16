9b343e418258c8f8a7441043b7f64e73d1938066
Andy <andy.dot.heng@gmail.com>
2018-02-16 18:03:23 -0500

    wrap map in div

--- src/component/TierRow.js
+++ src/component/TierRow.js
@@ -1,15 +1,17 @@
-import React, {Fragment} from "react";
+import React, { Fragment } from "react";
 import Ship from "./Ship";
 
 const TierRow = (props) => (
   <Fragment>
     <h2>{props.rowTitle}</h2>
-    {
-      // create ship item, sort by rarity, then sort by name
-      props.filteredDataByRows
-        .map(ship => (<Ship {...ship} key={ship._id}/>))
-        .sort((a, b) => b.props.rarity.localeCompare(a.props.rarity) || a.props.name.localeCompare(b.props.name))
-    }
+    <div>
+      {
+        // create ship item, sort by rarity, then sort by name
+        props.filteredDataByRows
+          .map(ship => (<Ship {...ship} key={ship._id} />))
+          .sort((a, b) => b.props.rarity.localeCompare(a.props.rarity) || a.props.name.localeCompare(b.props.name))
+      }
+    </div>
   </Fragment>
 )
 
