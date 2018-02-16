1cff2f83476fcefb2b10bbf49ea3d9b0337ed211
Andy <andy.dot.heng@gmail.com>
2018-02-16 18:24:38 -0500

    implement details and rarity filters

--- src/component/redux/selectors/ships.js
+++ src/component/redux/selectors/ships.js
@@ -1,7 +1,9 @@
 const showFilteredShips = (shipsArr, {text, nation, detail, rarity}) => {
   return shipsArr.filter(ship => {
     const nationFilter = ship.nation.toLowerCase().match(nation.toLowerCase());
-    return nationFilter;
+    const detailsFilter = detail.length === 0 || ship.details[detail];
+    const rarityFilter = rarity.length === 0 || rarity === ship.rarity;
+    return nationFilter && detailsFilter && rarityFilter;
   })
 }
 
