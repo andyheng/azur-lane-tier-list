a68c85c5890214b53e3e5ad0c5f493886772833d
Andy <andy.dot.heng@gmail.com>
2018-02-16 17:13:05 -0500

    pass filtered data down to single row component

--- src/component/TierRows.js
+++ src/component/TierRows.js
@@ -1,8 +1,21 @@
 import React from "react";
 import TierRow from "./TierRow";
 
-const TierRows = () => (
-  <TierRow />
-)
+const TierRows = (props) => {
+  const rowTiers = ["T0", "T1", "T2", "T3", "T4", "T5"];
+  return (
+    <div>
+      {
+        rowTiers.map(tier => (
+          <TierRow 
+            filteredDataByRows={props.filteredData.filter(ship => ship.tier === tier)}
+            key={tier}
+            rowTitle={tier}
+          />
+        ))
+      }
+    </div>
+  )
+}
 
 export default TierRows;
