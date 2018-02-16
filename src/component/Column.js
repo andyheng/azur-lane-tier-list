576973473d1badc9ef5e9fe201662c826845a852
Andy <andy.dot.heng@gmail.com>
2018-02-16 17:00:59 -0500

    Replace stateless component with class component connected to redux

--- src/component/Column.js
+++ src/component/Column.js
@@ -1,11 +1,27 @@
-import React, {Fragment} from "react";
+import React, { Fragment } from "react";
 import TierRows from "./TierRows";
+// redux
+import { connect } from "react-redux";
+import { shipsFetchData } from "./redux/actions/ships";
 
-const Column = () => (
-  <Fragment>
-    <h1>Test columns:</h1>
-    <TierRows />
-  </Fragment>
-)
+class Column extends React.Component {
+  componentDidMount() {
+    this.props.dispatch(shipsFetchData("http://localhost:3000/api/ships"));
+  }
+  render() {
+    return (
+      <Fragment>
+        <h1>Test columns:</h1>
+        <TierRows />
+      </Fragment>
+    )
+  }
+}
 
-export default Column;
+const mapStateToprops = (state) => {
+  return {
+    ships: state.ships
+  }
+}
+
+export default connect(mapStateToprops)(Column);
