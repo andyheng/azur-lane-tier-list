24aef6015cc58d5da352af30384a24af484eaa15
Andy <andy.dot.heng@gmail.com>
2018-02-16 16:06:40 -0500

    Initialize app

---
+++ src/app.js
@@ -0,0 +1,11 @@
+// React
+import React from "react";
+import ReactDOM from "react-dom";
+import ALTierApp from "./component/ALTierApp";
+// Style
+import "normalize.css/normalize.css";
+
+ReactDOM.render(
+  <ALTierApp />,
+  document.querySelector("#container")
+)
