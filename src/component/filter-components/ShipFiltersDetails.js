dacdc8d969c06e0ac5536b356df71afbd6e65711
Andy <andy.dot.heng@gmail.com>
2018-02-17 00:47:48 -0500

    implement details button map

--- src/component/filter-components/ShipFiltersDetails.js
+++ src/component/filter-components/ShipFiltersDetails.js
@@ -1,11 +1,41 @@
 import React, { Fragment } from "react";
 import ShipFiltersDetailsMap from "./ShipFiltersDetailsMap";
+import { connect } from "react-redux";
+import { setDetailsFilter, setDetailsFilterVisiblity, clearFilter } from "../redux/actions/filters";
+import { CSSTransitionGroup } from "react-transition-group";
 
-const ShipFiltersDetails = () => (
-  <div>
-    <h1>Test Filters: Details</h1>
-  <ShipFiltersDetailsMap />
-  </div>
+const ShipFiltersDetails = (props) => {
+  const showAndClearFilter = () => {
+    props.filters.details === "" ?
+      props.dispatch(setDetailsFilterVisiblity()) :
+      props.dispatch(clearFilter(setDetailsFilter, setDetailsFilterVisiblity));
+  }
+  return (
+    <section>
+      <button
+        onClick={() => showAndClearFilter()}
+        className={props.filters.detailsVisible ? "filter__handler-btn filter__handler-btn--active" : "filter__handler-btn"}
+      >
+        <span>Details</span>
+        {props.filters.detailsVisible ? <span>✖</span> : <span>▼</span>}
+      </button>
+
+      <CSSTransitionGroup
+        transitionName="transitionFilter"
+        transitionEnterTimeout={400}
+        transitionLeaveTimeout={400}
+        component={Fragment}
+      >
+        {props.filters.detailsVisible && <ShipFiltersDetailsMap />}
+      </CSSTransitionGroup>
+    </section>
+  )
+}
+
+const mapStateToProps = (state) => (
+  {
+    filters: state.filters
+  }
 )
 
-export default ShipFiltersDetails
+export default connect(mapStateToProps)(ShipFiltersDetails)
