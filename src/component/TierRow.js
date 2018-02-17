da75b0eb363befd17ac3a8190e9430402ac81f1e
Andy <andy.dot.heng@gmail.com>
2018-02-16 22:50:22 -0500

    implement cssTransitionGroup

--- src/component/TierRow.js
+++ src/component/TierRow.js
@@ -1,17 +1,24 @@
 import React from "react";
 import Ship from "./Ship";
+import { CSSTransitionGroup } from "react-transition-group";
 
 const TierRow = (props) => (
   <div className="row">
     <h2 className="row__title">{props.rowTitle}</h2>
-    <div className="row__items">
-      {
-        // create ship item, sort by rarity, then sort by name
-        props.filteredDataByRows
-          .map(ship => (<Ship {...ship} key={ship._id} />))
-          .sort((a, b) => b.props.rarity.localeCompare(a.props.rarity) || a.props.name.localeCompare(b.props.name))
-      }
-    </div>
+      <CSSTransitionGroup 
+        transitionName="transitionFilter" 
+        transitionEnterTimeout={400}
+        transitionLeaveTimeout={400}
+        component="div" 
+        className="row__items"
+      >
+        {
+          // create ship item, sort by rarity, then sort by name
+          props.filteredDataByRows
+            .map(ship => (<Ship {...ship} key={ship._id} />))
+            .sort((a, b) => b.props.rarity.localeCompare(a.props.rarity) || a.props.name.localeCompare(b.props.name))
+        }
+      </CSSTransitionGroup>
   </div>
 )
 
