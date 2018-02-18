8cd49aa444134b405158f3b2b57826f720f17a61
Andy <andy.dot.heng@gmail.com>
2018-02-18 14:25:20 -0500

    revert to commit 6ad847

--- webpack.config.js
+++ webpack.config.js
@@ -1,57 +1,43 @@
 const path = require("path");
-const ExtractTextPlugin = require("extract-text-webpack-plugin");
 
-module.exports = (env) => {
-  const isProduction = env === "production";
-  const CSSExtract = new ExtractTextPlugin("style.css");
-  return {
-    entry: "./src/app.js",
-    output: {
-      path: path.join(__dirname, "public"),
-      filename: "bundle.js"
+// First, we need the entry and output points
+module.exports = {
+  entry: "./src/app.js",
+  output: {
+    path: path.join(__dirname, "public"),
+    filename: "bundle.js"
+  },
+  module: {
+    rules: [{
+      loader: "babel-loader",
+      test: /\.js$/,
+      exclude: /node_modules/
     },
-    module: {
-      rules: [{
-        loader: "babel-loader",
-        test: /\.js$/,
-        exclude: /node_modules/
-      },
-      {
-        test: /\.(jpe|jpg|woff|woff2|eot|ttf|svg|png|gif)(\?.*$|$)/,
-        use: [
-          {
-            loader: "url-loader",
-            options: {
-              limit: 8192
-            }
+    {
+      test: /\.(jpe|jpg|woff|woff2|eot|ttf|svg|png|gif)(\?.*$|$)/,
+      use: [
+        {
+          loader: "url-loader",
+          options: {
+            limit: 8192
           }
-        ]
-      },
-      {
-        test: /\.s?css$/,
-        use: CSSExtract.extract({
-          use: [
-            {
-              loader: "css-loader", 
-              options: {sourceMap: true}
-            },
-            {
-              loader: "sass-loader",
-              options: {sourceMap: true}
-            }
-          ]
-        })
-      }]
+        }
+      ]
     },
-    plugins: [
-      CSSExtract
-    ],
-    devtool: isProduction ? "source-map" : "inline-source-map",
-    devServer: {
-      contentBase: path.join(__dirname, "public"),
-      port: 8080,
-      historyApiFallback: true
-    }
+  {
+    test: /\.s?css$/,
+    use: [
+      "style-loader",
+      "css-loader",
+      "sass-loader"
+    ]
+  }]
+  },
+  devtool: "cheap-module-eval-source-map",
+  devServer: {
+    contentBase: path.join(__dirname, "public"),
+    port: 8080,
+    historyApiFallback: true
   }
 }
 
