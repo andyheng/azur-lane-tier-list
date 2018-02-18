3988d23dead839d14358fb30abdd64ef4c1412f6
Andy <andy.dot.heng@gmail.com>
2018-02-18 12:22:06 -0500

    Set up webpack.config for production

--- webpack.config.js
+++ webpack.config.js
@@ -1,43 +1,57 @@
 const path = require("path");
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
 
-// First, we need the entry and output points
-module.exports = {
-  entry: "./src/app.js",
-  output: {
-    path: path.join(__dirname, "public"),
-    filename: "bundle.js"
-  },
-  module: {
-    rules: [{
-      loader: "babel-loader",
-      test: /\.js$/,
-      exclude: /node_modules/
+module.exports = (env) => {
+  const isProduction = env === "production";
+  const CSSExtract = new ExtractTextPlugin("style.css");
+  return {
+    entry: "./src/app.js",
+    output: {
+      path: path.join(__dirname, "public"),
+      filename: "bundle.js"
     },
-    {
-      test: /\.(jpe|jpg|woff|woff2|eot|ttf|svg|png|gif)(\?.*$|$)/,
-      use: [
-        {
-          loader: "url-loader",
-          options: {
-            limit: 8192
+    module: {
+      rules: [{
+        loader: "babel-loader",
+        test: /\.js$/,
+        exclude: /node_modules/
+      },
+      {
+        test: /\.(jpe|jpg|woff|woff2|eot|ttf|svg|png|gif)(\?.*$|$)/,
+        use: [
+          {
+            loader: "url-loader",
+            options: {
+              limit: 8192
+            }
           }
-        }
-      ]
+        ]
+      },
+      {
+        test: /\.s?css$/,
+        use: CSSExtract.extract({
+          use: [
+            {
+              loader: "css-loader", 
+              options: {sourceMap: true}
+            },
+            {
+              loader: "sass-loader",
+              options: {sourceMap: true}
+            }
+          ]
+        })
+      }]
     },
-  {
-    test: /\.s?css$/,
-    use: [
-      "style-loader",
-      "css-loader",
-      "sass-loader"
-    ]
-  }]
-  },
-  devtool: "cheap-module-eval-source-map",
-  devServer: {
-    contentBase: path.join(__dirname, "public"),
-    port: 8080,
-    historyApiFallback: true
+    plugins: [
+      CSSExtract
+    ],
+    devtool: isProduction ? "source-map" : "inline-source-map",
+    devServer: {
+      contentBase: path.join(__dirname, "public"),
+      port: 8080,
+      historyApiFallback: true
+    }
   }
 }
 
