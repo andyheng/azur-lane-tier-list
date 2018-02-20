33edf57e4f70697d853e5f748bce67cb71202d8f
Andy <andy.dot.heng@gmail.com>
2018-02-20 16:39:35 -0500

    implement local state for tracking and hiding DOM while filtering

--- src/component/TierRows.js
+++ src/component/TierRows.js
@@ -1,21 +1,63 @@
-import React from "react";
+import React, {Fragment} from "react";
 import TierRow from "./TierRow";
+import {connect} from "react-redux"
+import { CSSTransitionGroup } from "react-transition-group";
 
-const TierRows = (props) => {
-  const rowTiers = ["T0", "T1", "T2", "T3", "T4", "T5"];
-  return (
-    <div>
-      {
-        rowTiers.map(tier => (
-          <TierRow 
-            filteredDataByRows={props.filteredData.filter(ship => ship.tier === tier)}
-            key={tier}
-            rowTitle={tier}
-          />
-        ))
-      }
-    </div>
-  )
+class TierRows extends React.Component {
+  state = {
+    isLoading: false
+  }
+  handleState() {
+    this.setState(() => ({isLoading: !this.state.isLoading}), this.delayState )
+  }
+  delayState() {
+    setTimeout(() => {
+      this.setState(() => ({
+        isLoading: false
+      }))
+    }, 400)
+  }
+  componentWillReceiveProps(nextProps) {
+    if (JSON.stringify(nextProps.filteredData) !== JSON.stringify(this.props.filteredData)) {
+      this.handleState()
+    }
+  }
+  setLoadingClass() {
+    let className = "spinner";
+    if (this.props.position === "Back") {
+      className += " spinner-left"
+    } else {
+      className += " spinner-right"
+    }
+    return className;
+  }
+  
+  render() {
+    const rowTiers = ["T0", "T1", "T2", "T3", "T4", "T5"];
+    return (
+      <div>
+        <CSSTransitionGroup
+        transitionName="transitionFilter"
+        transitionEnterTimeout={400}
+        transitionLeaveTimeout={400}
+        component={Fragment}
+      >
+        {
+          this.state.isLoading ? null :
+            rowTiers.map(tier => (
+              <TierRow 
+                filteredDataByRows={this.props.filteredData.filter(ship => ship.tier === tier)}
+                key={tier}
+                rowTitle={tier}
+              />
+            ))
+        }
+        </CSSTransitionGroup>
+      </div>
+    )
+  }
 }
 
-export default TierRows;
+const mapStateToProps = (state) => ({filters: state.filters})
+
+export default connect(mapStateToProps)(TierRows);
