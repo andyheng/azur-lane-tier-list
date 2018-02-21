672b39df7860cc290e1a6cff522c9b05c78e009a
Andy <andy.dot.heng@gmail.com>
2018-02-21 13:47:56 -0500

    replace filterArrs with appArrs, which exports an object w/ arrays

---
+++ src/component/appArrs.js
@@ -0,0 +1,6 @@
+export const appArrs = {
+  details: ["additionalGun", "torpedo", "barrage", "antiAir", "survival", "recovery", "persistentBuff", "activationBuff", "retrofit", "limited"],
+  nations: ["Sakura Empire", "Eagle Union", "Royal Navy", "MetalBlood", "Eastern Radiance", "North Union"],
+  rarity: ["N", "R", "SR", "SSR"],
+  tiers: ["T0", "T1", "T2", "T3", "T4", "T5"]
+}
--- src/component/filter-components/filterArrs.js
+++
@@ -1,5 +0,0 @@
-export const detailsAll = ["additionalGun", "torpedo", "barrage", "antiAir", "survival", "recovery", "persistentBuff", "activationBuff", "retrofit", "limited"];
-
-export const nationsAll = ["Sakura Empire", "Eagle Union", "Royal Navy", "MetalBlood", "Eastern Radiance", "North Union"];
-
-export const rarityAll = ["N", "R", "SR", "SSR"];
