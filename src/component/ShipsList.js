b243eae795157534b3bfa23300d7f7f41202533b
Andy <andy.dot.heng@gmail.com>
2018-02-22 17:25:22 -0500

    Implement column visible state

--- src/component/ShipsList.js
+++ src/component/ShipsList.js
@@ -1,11 +1,28 @@
-import React from "react";
+import React, { Fragment } from "react";
 import Column from "./Column";
+import { connect } from "react-redux";
 
-const ShipsList = () => (
-  <main className="content">
-    <Column position={"Back"} />
-    <Column position={"Front"} />
-  </main>
+const ShipsList = (props) => {
+  return (
+    <main className="content">
+      {
+        props.column.isFrontVisible ?
+          <Fragment>
+            <Column position={"Front"} />
+          </Fragment>
+          :
+          <Fragment>
+            <Column position={"Back"} />
+          </Fragment>
+      }
+    </main>
+  )
+}
+
+const mapStateToProps = (state) => (
+  {
+    column: state.column
+  }
 )
 
-export default ShipsList;
+export default connect(mapStateToProps)(ShipsList);
